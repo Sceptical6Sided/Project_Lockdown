df6a3be9e9a0fc80752f60e68e5fbf5777a509ac
Sceptical6Sided <38331084+Sceptical6Sided@users.noreply.github.com>
2024-03-21 18:26:22 +0100

    Switch include order to the correct one

--- Source/Project_Lockdown/Private/CustomCharacter.cpp
+++ Source/Project_Lockdown/Private/CustomCharacter.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "CustomCharacterMovementComponent.h"
 #include "CustomCharacter.h"
+#include "CustomCharacterMovementComponent.h"
 
 // Sets default values
 ACustomCharacter::ACustomCharacter()
