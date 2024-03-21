1a75694044ad4ca0f1d9587aea3d721cd9787ce7
Sceptical6Sided <38331084+Sceptical6Sided@users.noreply.github.com>
2024-03-21 18:34:40 +0100

    Implemented SavedMove from previous project

--- Source/Project_Lockdown/Private/CustomCharacterMovementComponent.cpp
+++ Source/Project_Lockdown/Private/CustomCharacterMovementComponent.cpp
@@ -5,31 +5,87 @@
 
 #pragma region SavedMove
 
-bool UCustomCharacterMovementComponent::FSavedMove_Custom::CanCombineWith(const FSavedMovePtr& NewMove,
-	ACharacter* InCharacter, float MaxDelta) const
+UCustomCharacterMovementComponent::FSavedMove_Custom::FSavedMove_Custom()
 {
+	Saved_bWantsToSprint=0;
+	Saved_bWantsToProne = 0;
+	Saved_bWantsToWalk = 0;
+	Saved_bPrevWantsToCrouch=0;
+	Saved_bPressedCustomJump=0;
+	Saved_bHadAnimRootMotion=0;
+	Saved_bTransitionFinished=0;
+}
+
+bool UCustomCharacterMovementComponent::FSavedMove_Custom::CanCombineWith(const FSavedMovePtr& NewMove, ACharacter* InCharacter, float MaxDelta) const
+{
+	FSavedMove_Custom* NewCustomMove = static_cast<FSavedMove_Custom*>(NewMove.Get());
+	if(Saved_bWantsToSprint != NewCustomMove->Saved_bWantsToSprint)
+	{
+		return false;
+	}
+
 	return FSavedMove_Character::CanCombineWith(NewMove, InCharacter, MaxDelta);
 }
 
 void UCustomCharacterMovementComponent::FSavedMove_Custom::Clear()
 {
 	FSavedMove_Character::Clear();
+
+	Saved_bWantsToSprint = 0;
+
+	Saved_bWantsToProne = 0;
+	Saved_bPrevWantsToCrouch = 0;
+
+	Saved_bPressedCustomJump = 0;
+	Saved_bHadAnimRootMotion = 0;
+	Saved_bTransitionFinished = 0;
 }
 
 uint8 UCustomCharacterMovementComponent::FSavedMove_Custom::GetCompressedFlags() const
 {
-	return FSavedMove_Character::GetCompressedFlags();
+	uint8 Result = Super::FSavedMove_Character::GetCompressedFlags();
+
+	if(Saved_bWantsToSprint) Result |= FLAG_Custom_0;
+	//High-jacking the Jump flag to lighten network bandwidth
+	if(Saved_bPressedCustomJump) Result |= FLAG_JumpPressed;
+
+	return Result;
 }
 
-void UCustomCharacterMovementComponent::FSavedMove_Custom::SetMoveFor(ACharacter* C, float InDeltaTime,
-	FVector const& NewAccel, FNetworkPredictionData_Client_Character& ClientData)
+void UCustomCharacterMovementComponent::FSavedMove_Custom::SetMoveFor(ACharacter* C, float InDeltaTime, FVector const& NewAccel, FNetworkPredictionData_Client_Character& ClientData)
 {
 	FSavedMove_Character::SetMoveFor(C, InDeltaTime, NewAccel, ClientData);
+
+	UCustomCharacterMovementComponent* CharacterMovement = Cast<UCustomCharacterMovementComponent>(C->GetCharacterMovement());
+
+	Saved_bWantsToSprint = CharacterMovement->Safe_bWantsToSprint;
+
+	Saved_bPrevWantsToCrouch = CharacterMovement->Safe_bPrevWantsToCrouch;
+
+	Saved_bWantsToProne = CharacterMovement->Safe_bWantsToProne;
+
+	Saved_bPressedCustomJump = CharacterMovement->CustomCharacterOwner->bPressedCustomJump;
+
+	Saved_bHadAnimRootMotion = CharacterMovement->Safe_bHadAnimRootMotion;
+	Saved_bTransitionFinished = CharacterMovement->Safe_bTransitionFinished;
 }
 
 void UCustomCharacterMovementComponent::FSavedMove_Custom::PrepMoveFor(ACharacter* C)
 {
 	FSavedMove_Character::PrepMoveFor(C);
+
+	UCustomCharacterMovementComponent* CharacterMovement = Cast<UCustomCharacterMovementComponent>(C->GetCharacterMovement());
+
+	CharacterMovement->Safe_bWantsToSprint = Saved_bWantsToSprint;
+
+	CharacterMovement->Safe_bPrevWantsToCrouch = Saved_bPrevWantsToCrouch;
+
+	CharacterMovement->Safe_bWantsToProne = Saved_bWantsToProne;
+
+	CharacterMovement->CustomCharacterOwner->bPressedCustomJump = Saved_bPressedCustomJump;
+
+	CharacterMovement->Safe_bHadAnimRootMotion = Saved_bHadAnimRootMotion;
+	CharacterMovement->Safe_bTransitionFinished = Saved_bTransitionFinished;
 }
 
 #pragma endregion
