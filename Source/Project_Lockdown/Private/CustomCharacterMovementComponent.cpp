fe25b432c03e2595609f1bd9923d753f024d8bc4
Sceptical6Sided <38331084+Sceptical6Sided@users.noreply.github.com>
2024-03-21 18:32:02 +0100

    Created the SavedMove Class and implementation in the cpp

--- Source/Project_Lockdown/Private/CustomCharacterMovementComponent.cpp
+++ Source/Project_Lockdown/Private/CustomCharacterMovementComponent.cpp
@@ -3,3 +3,33 @@
 
 #include "CustomCharacterMovementComponent.h"
 
+#pragma region SavedMove
+
+bool UCustomCharacterMovementComponent::FSavedMove_Custom::CanCombineWith(const FSavedMovePtr& NewMove,
+	ACharacter* InCharacter, float MaxDelta) const
+{
+	return FSavedMove_Character::CanCombineWith(NewMove, InCharacter, MaxDelta);
+}
+
+void UCustomCharacterMovementComponent::FSavedMove_Custom::Clear()
+{
+	FSavedMove_Character::Clear();
+}
+
+uint8 UCustomCharacterMovementComponent::FSavedMove_Custom::GetCompressedFlags() const
+{
+	return FSavedMove_Character::GetCompressedFlags();
+}
+
+void UCustomCharacterMovementComponent::FSavedMove_Custom::SetMoveFor(ACharacter* C, float InDeltaTime,
+	FVector const& NewAccel, FNetworkPredictionData_Client_Character& ClientData)
+{
+	FSavedMove_Character::SetMoveFor(C, InDeltaTime, NewAccel, ClientData);
+}
+
+void UCustomCharacterMovementComponent::FSavedMove_Custom::PrepMoveFor(ACharacter* C)
+{
+	FSavedMove_Character::PrepMoveFor(C);
+}
+
+#pragma endregion
--- Source/Project_Lockdown/Public/CustomCharacterMovementComponent.h
+++ Source/Project_Lockdown/Public/CustomCharacterMovementComponent.h
@@ -24,5 +24,30 @@ UCLASS()
 class PROJECT_LOCKDOWN_API UCustomCharacterMovementComponent : public UCharacterMovementComponent
 {
 	GENERATED_BODY()
+
+	class FSavedMove_Custom : public FSavedMove_Character
+	{
+		typedef FSavedMove_Character Super;
+		//Flag
+		uint8 Saved_bWantsToSprint:1;
+		uint8 Saved_bWantsToWalk:1;
+		uint8 Saved_bPressedCustomJump:1; //"PseudoFlag" for mantle mechanic
+
+		//Non-Flag
+		uint8 Saved_bPrevWantsToCrouch:1;
+		uint8 Saved_bWantsToProne:1;
+		uint8 Saved_bHadAnimRootMotion:1;
+		uint8 Saved_bTransitionFinished:1;
+
+	public:
+	
+		FSavedMove_Custom();
+	
+		virtual bool CanCombineWith(const FSavedMovePtr& NewMove, ACharacter* InCharacter, float MaxDelta) const override;
+		virtual void Clear() override;
+		virtual uint8 GetCompressedFlags() const override;
+		virtual void SetMoveFor(ACharacter* C, float InDeltaTime, FVector const& NewAccel, FNetworkPredictionData_Client_Character& ClientData) override;
+		virtual void PrepMoveFor(ACharacter* C) override;
+	};
 	
 };
