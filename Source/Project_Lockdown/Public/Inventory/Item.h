6c30fd12c7f3157e204fcba37b9ce5921861ecca
Sceptical6Sided <38331084+Sceptical6Sided@users.noreply.github.com>
2024-03-23 10:23:23 +0100

    Sets basic props for item class

--- Source/Project_Lockdown/Public/Inventory/Item.h
+++ Source/Project_Lockdown/Public/Inventory/Item.h
@@ -3,7 +3,6 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "UObject/NoExportTypes.h"
 #include "Item.generated.h"
 
 /**
@@ -14,4 +13,29 @@ class PROJECT_LOCKDOWN_API UItem : public UObject
 {
 	GENERATED_BODY()
 	
+public:
+
+	UItem();
+
+	//Used for tooltip
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Item") FText UseActionText;
+
+	//Used for 3d representation in world
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Item") class UStaticMesh* PickUpMesh;
+
+	//Used for Item Thumbnail in Inventory
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Item") UTexture2D* Thumbnail;
+
+	//Used for item name for inventory
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Item") FText ItemDisplayName;
+
+	//Optional description for the item
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Item", meta=(MultiLine = true)) FText ItemDescription;
+
+	//Weight for the item
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Item", meta=(ClampMin=0.0)) float Weight;
+
+	//Inventory that owns the item
+	UPROPERTY()
+	class UInventoryComponent* OwningInventory;
 };
