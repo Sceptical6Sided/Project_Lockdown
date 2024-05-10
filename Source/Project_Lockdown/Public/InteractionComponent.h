734f04e106c719f570049bb0fe0f7cff9996e163
Sceptical6Sided <38331084+Sceptical6Sided@users.noreply.github.com>
2024-05-10 10:51:18 +0200

    Creates InteractionComponenet
    Creates C++ WidgetComponent for the interaction system to be implemented to

---
+++ Source/Project_Lockdown/Public/InteractionComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/WidgetComponent.h"
+#include "InteractionComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_LOCKDOWN_API UInteractionComponent : public UWidgetComponent
+{
+	GENERATED_BODY()
+	
+};
