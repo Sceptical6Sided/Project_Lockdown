6397973800bc67d9c5269ab2a8779e49f52518d0
Sceptical6Sided <38331084+Sceptical6Sided@users.noreply.github.com>
2024-05-10 10:40:36 +0200

    Create UE_GameplayFramework C++ classes

---
+++ Source/Project_Lockdown/Public/Framework/CustomGameStateBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "CustomGameStateBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_LOCKDOWN_API ACustomGameStateBase : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+};
