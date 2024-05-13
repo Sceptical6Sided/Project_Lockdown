e260ca793e10b960c34fd47678602d0977b5c237
Sceptical6Sided <38331084+Sceptical6Sided@users.noreply.github.com>
2024-05-13 14:22:24 +0200

    Update Item
    Expand Item class to better use the new inventory system and replication

--- Source/Project_Lockdown/Private/Inventory/Item.cpp
+++ Source/Project_Lockdown/Private/Inventory/Item.cpp
@@ -3,12 +3,47 @@
 
 #include "Inventory/Item.h"
 
+#include "Net/UnrealNetwork.h"
+
+#define LOCTEXT_NAMESPACE "Item"
+
+void UItem::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	UObject::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(UItem, Quantity);
+}
+
+bool UItem::IsSupportedForNetworking() const
+{
+	return true;
+}
+
 UItem::UItem()
 {
-	Weight = 1.f;
-	ItemDisplayName = FText::FromString("Item");
-	UseActionText = FText::FromString("Use");
-	ItemType = EItemType::IT_Junk;
-	bIsThrowable = false;
+	ItemDisplayName = LOCTEXT("ItemName","Item");
+	UseActionText = LOCTEXT("ItemActionText","Use");
+	Weight = 0.f;
+	bStackable = true;
+	Quantity = 1;
+	MaxStackSize = 2;
+	RepKey = 0;
+	ItemType = EItemType::It_Junk;
 	bIsSpecialItem=false;
 }
+
+void UItem::OnRep_Quantity()
+{
+}
+
+bool UItem::ShouldShowInInventory() const
+{
+	return true;
+}
+
+void UItem::MarkDirtyForReplication()
+{
+	
+}
+
+#undef LOCTEXT_NAMESPACE
--- Source/Project_Lockdown/Public/Inventory/Item.h
+++ Source/Project_Lockdown/Public/Inventory/Item.h
@@ -9,21 +9,26 @@
 UENUM()
 enum EItemType
 {
-	IT_Junk			UMETA(DispalyName = "Junk"),
-	IT_Clothing		UMETA(DispalyName = "Clothing"),
-	IT_Container	UMETA(DispalyName = "Container"),
-	IT_Food			UMETA(DisplayName = "Food"),
-	IT_Armor		UMETA(DisplayName = "Armor"),
-	IT_Weapon		UMETA(DispalyName = "Weapon"),
-	IT_Magazine		UMETA(DisplayNme = "Magazine"),
-	IT_Ammo			UMETA(DisplayName = "Ammo"),
-	IT_Meds			UMETA(DispalyName = "Meds"),
+	It_Junk			UMETA(DisplayName = "Junk"),
+	It_Clothing		UMETA(DisplayName = "Clothing"),
+	It_Container	UMETA(DispalyName = "Container"),
+	It_Food			UMETA(DisplayName = "Food"),
+	It_Armor		UMETA(DisplayName = "Armor"),
+	It_Weapon		UMETA(DisplayName = "Weapon"),
+	It_Magazine		UMETA(DisplayNme = "Magazine"),
+	It_Ammo			UMETA(DisplayName = "Ammo"),
+	It_Meds			UMETA(DisplayName = "Meds"),
 };
 
-UCLASS(Abstract, BlueprintType, Blueprintable)
+UCLASS(Abstract, BlueprintType, Blueprintable, EditInlineNew, DefaultToInstanced)
 class PROJECT_LOCKDOWN_API UItem : public UObject
 {
 	GENERATED_BODY()
+
+protected:
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	virtual bool IsSupportedForNetworking() const override;
 	
 public:
 
@@ -49,28 +54,44 @@ public:
 	//Optional description for the item
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Item", meta=(MultiLine = true)) FText ItemDescription;
 
+	//Whether this item can be stacked
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Item") bool bStackable;
+
+	//Maximum stack size if the item can be stacked
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Item", meta = (ClampMin = 2, EditCondition = bStackable)) int32 MaxStackSize;
+	
 	//Weight for the item
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Item", meta=(ClampMin=0.0)) float Weight;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Item", meta = (ClampMin = 0.0)) float Weight;
 
 	//Indicator flag to know if the item is a "special item" or not, use for quest items and such
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Item") bool bIsSpecialItem;
+	
+	//The tooltip widget for the item
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Item") TSubclassOf<class UItemTooltipWidget> ItemTooltip;
+
+	//Server managed variable for the amount of the item
+	UPROPERTY( ReplicatedUsing = OnRep_Quantity, EditAnywhere, Category="Item", meta = (UIMin = 1, EditCondition = bStackable)) int32 Quantity;
 
 	//Inventory that owns the item
 	UPROPERTY() class UInventoryComponent* OwningInventory;
 
-#pragma region Throwing
+	//Used to efficiently replicate inventory items
+	UPROPERTY() int32 RepKey;
 
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Throw", meta=(ClampMin=0.0)) float InitialSpeed = 800;
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Throw", meta=(ClampMin=0.0)) float MaxSpeed = 8500;
+	UFUNCTION()
+	void OnRep_Quantity();
 	
-	//Flag to tell the system if the item is throwable
-	UPROPERTY(EditDefaultsOnly, Category="Throw")
-	bool bIsThrowable;
-	
-#pragma endregion
-
 	virtual void Use(class ACustomCharacter* Character) PURE_VIRTUAL(UItem,);
 
 	UFUNCTION(BlueprintImplementableEvent)
 	void OnUse(ACustomCharacter* Character);
+
+	UFUNCTION(BlueprintCallable, Category="Item")
+	FORCEINLINE float GetStackWeight() const {return Quantity * Weight; }
+
+	UFUNCTION(BlueprintPure, Category="Item")
+	virtual bool ShouldShowInInventory() const;
+
+	//Mark the object as needing replication. We must call this internally after modifying any replicated properties
+	void MarkDirtyForReplication();
 };
