0cd4be318ad14455fe65797b3057ff378a9b9571
Sceptical6Sided <38331084+Sceptical6Sided@users.noreply.github.com>
2024-05-13 14:48:32 +0200

    Update FoodItem.cpp
    Updates food item with localization support

--- Source/Project_Lockdown/Private/Inventory/FoodItem.cpp
+++ Source/Project_Lockdown/Private/Inventory/FoodItem.cpp
@@ -3,11 +3,13 @@
 
 #include "Inventory/FoodItem.h"
 
-
+#define LOCTEXT_NAMESPACE "FoodItem"
 UFoodItem::UFoodItem()
 {
 	ItemType = It_Food;
 	FoodType = FT_Dry;
+	UseActionText = LOCTEXT("ItemUseActionText", "Consume");
+	HealAmount = 20.f;
 }
 
 void UFoodItem::Use(ACustomCharacter* Character)
@@ -17,3 +19,4 @@ void UFoodItem::Use(ACustomCharacter* Character)
 		Character->Health += HealAmount;
 	}
 }
+#undef LOCTEXT_NAMESPACE
