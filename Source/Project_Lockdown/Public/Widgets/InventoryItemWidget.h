3f825c73a06384d3246913012aa4e5b6a3cec2f3
Sceptical6Sided <38331084+Sceptical6Sided@users.noreply.github.com>
2024-05-14 11:09:09 +0200

    Create InventoryItemWidget
    Create parent class for the InventoryItem to in herit from, for custom C++ code

---
+++ Source/Project_Lockdown/Public/Widgets/InventoryItemWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "InventoryItemWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_LOCKDOWN_API UInventoryItemWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
