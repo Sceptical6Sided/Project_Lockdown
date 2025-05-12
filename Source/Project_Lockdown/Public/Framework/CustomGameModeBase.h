a9b0381d4f83904dd89d5082b470a60e4403caf1
Sceptical6Sided <38331084+Sceptical6Sided@users.noreply.github.com>
2025-05-12 17:47:04 +0200

    Update CustomGameModeBase.cpp

--- Source/Project_Lockdown/Public/Framework/CustomGameModeBase.h
+++ Source/Project_Lockdown/Public/Framework/CustomGameModeBase.h
@@ -4,14 +4,22 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
+#include "GameLiftServerSDK.h"
 #include "CustomGameModeBase.generated.h"
 
-/**
- * 
- */
+DECLARE_LOG_CATEGORY_EXTERN(GameServerLog, Log, All);
+
 UCLASS()
 class PROJECT_LOCKDOWN_API ACustomGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
-	
+public:
+	ACustomGameModeBase();
+protected:
+	virtual void BeginPlay() override;
+private:
+	//parameters that need to stay consistent during gameplay
+	FProcessParameters m_params;
+
+	void InitGameLift();
 };
