9e68d7423f516ca3fd2e41e21fb38b76d50ae6fb
Sceptical6Sided <38331084+Sceptical6Sided@users.noreply.github.com>
2025-05-12 15:20:20 +0200

    Integrated GameLiftPlugin

--- Source/Project_Lockdown/Project_Lockdown.Build.cs
+++ Source/Project_Lockdown/Project_Lockdown.Build.cs
@@ -8,7 +8,7 @@ public class Project_Lockdown : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "GameLiftServerSDK" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
